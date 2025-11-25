3c043ab83d3b2777dfaa70bdaed4d55235e9886c
puhachyeser <yehorpuhach@gmail.com>
2025-11-25 15:52:39 +0200

    add env variables in server dir

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,20 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { NotesModule } from './notes/notes.module';
-import { Note } from './notes/note.entity';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { NotesModule } from "./notes/notes.module";
+import { Note } from "./notes/note.entity";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'notes_db',
+      type: "postgres",
+      url: process.env.DB_URL,
       entities: [Note],
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== "production",
     }),
     NotesModule,
   ],
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
